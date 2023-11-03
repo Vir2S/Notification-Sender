3ba64ee64ae88c28f44b894fe14553bb91deb30f
vsem <virtualik@gmail.com>
2023-11-04 00:14:36 +0100

    swagger added

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 
 
 API_ROOT = "api/v1"
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Sender API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        f"{API_ROOT}/docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
     path(f"{API_ROOT}/auth/", include("authentication.urls")),
     path(f"{API_ROOT}/users/", include("users.urls")),
 ]
