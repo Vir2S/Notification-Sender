71a3d095b26cd2f56fec4743d1943c0eb58a7893
vsem <virtualik@gmail.com>
2023-11-05 17:23:46 +0100

    notifications api added

--- src/notifications/api.py
+++ src/notifications/api.py
@@ -1,8 +1,36 @@
 from rest_framework import viewsets
+
 from notifications.models import Notification
+from notifications.permissions import RoleIsAdmin, RoleIsManager, RoleIsUser, IsOwner
 from notifications.serializers import NotificationSerializer
+from users.constants import Role
 
 
 class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role == Role.ADMIN:
+            return Notification.objects.all()
+
+        return Notification.objects.filter(user=user)
+
+    def get_permissions(self):
+        match self.action:
+            case "list":
+                permission_classes = [RoleIsAdmin | RoleIsManager | RoleIsUser]
+            case "create":
+                permission_classes = [RoleIsAdmin | RoleIsUser]
+            case "retrieve":
+                permission_classes = [IsOwner | RoleIsAdmin | RoleIsManager]
+            case "update":
+                permission_classes = [IsOwner | RoleIsAdmin | RoleIsManager]
+            case "destroy":
+                permission_classes = [IsOwner | RoleIsAdmin | RoleIsManager]
+            case _:
+                permission_classes = []
+
+        return [permission() for permission in permission_classes]
