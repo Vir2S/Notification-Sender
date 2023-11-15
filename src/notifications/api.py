196bbf34fcc0070de11434f74c3780003307a5bf
vsem <virtualik@gmail.com>
2023-11-15 01:21:34 +0100

    update notification added

--- src/notifications/api.py
+++ src/notifications/api.py
@@ -78,3 +78,43 @@ class NotificationViewSet(viewsets.ModelViewSet):
             task_id=task_id,
             eta=scheduled_time
         )
+
+    def perform_update(self, serializer):
+        user = self.request.user
+        instance = serializer.instance
+
+        if not user.is_authenticated or (user != instance.user and user.role not in [Role.ADMIN, Role.MANAGER]):
+            return Response(
+                {
+                    "error": "You don't have permission to update this notification",
+                },
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        new_task_id = serializer.validated_data.get("task_id", None)
+        old_task_id = instance.task_id
+
+        updated_instance = serializer.save()
+
+        if new_task_id and new_task_id != old_task_id:
+            cancel_celery_task(old_task_id)
+
+        if instance.scheduled_send_date or instance.title or instance.message or instance.user:
+            recipient = updated_instance.user.email
+            subject = updated_instance.title
+            message = updated_instance.message
+            scheduled_time = updated_instance.scheduled_send_date
+
+            instance_to_task = {
+                "id": instance.id,
+                "recipient": recipient,
+                "subject": subject,
+                "message": message,
+                "scheduled_time": scheduled_time
+            }
+
+            send_scheduled_notification_task.apply_async(
+                (instance_to_task,),
+                task_id=new_task_id,
+                eta=scheduled_time
+            )
